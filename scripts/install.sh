40bf5d88895b7475fc6d59e5915cb0ee26af3a9e
Eetu Korhonen <eetu.korhonen@gmail.com>
2011-10-13 20:40:00 +0200

    edited pgsql configuration file

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,13 +4,15 @@
 # patch, readline-devel, zlib-devel, openssl-devel, java-1.6.0-openjdk-devel
 # groupinstall 'Development Tools'
 
+# GORDA needs to be made and installed before
+
 echo "Downloading and patching Postgresql+Gorda"
 echo "-----"
 ./download_gorda_psql.sh
 echo "Packages complete\n-----"
 # move to psql dir and start compling
 cd ./postgresql-8.1.3/
-./configure
+./configure --enable-depend --prefix=$gorda/install
 make
 # can't run default install without sudo
 # make install
