4ba6dde19207cc4f95476830f0e0d50cad71a029
Eetu Korhonen <eetu.korhonen@gmail.com>
2011-12-14 23:53:55 +0100

    The first prototypes of sql query parser regexes

--- src/python/log_parser.py
+++ src/python/log_parser.py
@@ -9,9 +9,14 @@ import time
 import re
 
 def parse_line(line):
-    result = re.search("SELECT cur_id", line)
-    if result:
-        print "*HEY, WE HAVE SELECT cur_id HERE!*"
+    if re.search(r"\s.+Query.+SELECT.+FROM.+", line, flags=re.IGNORECASE):
+        print "*SELECT QUERY*"
+    elif re.search(r"\s.+Query.+INSERT.+INTO.+", line, flags=re.IGNORECASE):
+        
+        table_name = re.search(r"insert.+into\s*(`|\"|')\S+(`|\"|')", line, flags =re.IGNORECASE).group(0).split().pop().strip("\"\'\`")
+        print "INSERT QUERY TO " + table_name
+    elif re.search(r"\s.+Query.+UPDATE.+SET.+", line, flags=re.IGNORECASE):
+        print "*UPDATE QUERY*"
 
 def main():
     file = open('remote_log.txt','r')
@@ -23,7 +28,6 @@ def main():
             file.seek(where)
         else:
             parse_line(line)
-            print line, # already has newline
-
+            # print line, # already has newline
 main()
 
