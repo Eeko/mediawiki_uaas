8715c54718aca4b2205a35304bdc862e62ffa6c0
Eetu Korhonen <eetu.korhonen@gmail.com>
2011-12-29 22:34:15 +0100

    rethought the update parsing...

--- src/python/insert_parser.py
+++ src/python/insert_parser.py
@@ -1,4 +1,4 @@
-#!/bin/env python
+#!/usr/bin/env python
 # insert_parser.py
 # 2011, Eetu Korhonen
 # Reads a mysql query-log file and parses any INSERT INTO statements from individual query.
@@ -30,7 +30,7 @@ def cut_queries_from_file(file):
                 # TODO: maybe should do this part when one reads a "Quit" command
                 # DONE!
                 queries.append(query)
-                print str(parse_inserts(query))
+                # print str(parse_inserts(query))
                 query = line
             query = query + line
 
@@ -41,7 +41,7 @@ def parse_inserts(query):
     results = []    # supports multiple inserts within a query
     match = re.search(r"\s+Query.+INSERT\s+INTO.+\)\n", query, flags=(re.DOTALL|re.IGNORECASE))  # --- 
     if match:
-        inserts = re.split(r"INSERT\s+INTO", query, flags=re.IGNORECASE)    # might need better matching, since there may be words between insert and into in mysql.  Also, should not work if the content contains "INSERT INTO"
+        inserts = re.split(r"INSERT\s+INTO", query, flags=re.IGNORECASE)    # might need better matching, since there may be words between insert and into in mysql.  Also, should ensure operability if the content contains "INSERT INTO"
         inserts.pop(0)  # remove the trail
         it = iter(inserts)  # creates an iterator of inserts
         for item in it:
@@ -62,7 +62,7 @@ def parse_inserts(query):
             
             split_at_values = end_found.group(0).partition("VALUES")
             columns = split_at_values[0].split()[1].strip('\(\s\t\)').split(",")
-            print "Columns: " + str(len(columns)) + ": "+ str(columns)
+            # print "Columns: " + str(len(columns)) + ": "+ str(columns)
 
             # the hard part...
             # print "1 " + str(split_at_values[2])
@@ -76,27 +76,109 @@ def parse_inserts(query):
                     values[index+1] = item + "," + values[index+1]
                     values.pop(index)
                 
-            print "Values " + str(len(values)) + ": " + str(values)
+            # print "Values " + str(len(values)) + ": " + str(values)
             mapping = dict(zip(columns,values))
             results.append([target_table,mapping]) # adds the target table and final mapping to a results-list
             
     return results
 
+# Similar method to parse update-queries
+# required to detect updates to "cur"-table
+def parse_updates(query):
+    results = []
+    # a more defined regex, since we do not want to match articles containing UPDATE...SET... in their text.
+           
+    
+    match = re.search(r"\n\s+\d+\s+Query\s/\*\s\S+::\S+\s\*/\s+UPDATE\s`.+`\sSET.+\n", query, flags=(re.DOTALL)) 
+    if match:
+        #print "match"
+        # updates = re.split(r"\s+\d+\s+Query\s/\*\s\S+::\S+\s\*/\s+UPDATE\s`.+`\s", query)    # might need better matching, since there may be words between insert and into in mysql.  Also, should ensure operability if the content contains "UPDATE"
+
+        # updates.pop(0)
+        # updates.pop(0)
+        # updates = "".join(updates)
+        print "Updates: {** " + str(query) + " **}"
+        it =  query.split("\n")
+        
+        found_queries = []
+        for index, item in enumerate(it):
+        #for index, item in enumerate(it):
+            # first, we need to find how many update-queries are within the parsed query
+            a_full_command = count_brackets(item)
+            #check if query begins with a proper UPDATE-statement and it does not leave open brackets
+            if re.search(r"\s+\d+\s+Query\s/\*\s\S+::\S+\s\*/\s+UPDATE\s`.+`\s", item) and a_full_command:
+                found_queries.append(item)
+                print "appended " + item + "\n"
+            elif re.search(r"\s+\d+\s+Query\s/\*\s\S+::\S+\s\*/\s+UPDATE\s`.+`\s", item):
+                itemtemp = it[index+ 1]
+                it[index + 1] = item + "\n" + itemtemp
+                print "extended " + it[index+1]
+        print "found_queries = " + str(found_queries) + "\n"
+            #target_table = re.search(r"\s*(`|\"|')\S+(`|\"|')", item.partition("SET")[0]).group(0).split().pop().strip("\"\'\`")
+            # print "Target table: " + target_table
+            # HACK! HACK! HACK! HACK!
+            # if target_table == "site_stats":
+            #     break
+            
+"""
+            mapping = {}
+            values = split_at_set[2].strip().split(",")
+            for index, item in enumerate(values):
+                column = item.partition("=")[0]
+                if not check_for_complete_parameter(item.partition("=")[2]):
+
+                    values[index+1] = item + "," + values[index+1]
+                    values.pop(index)
+                else:
+                    mapping[column] = item.partition("=")[2]
+                
+            # print "Values " + str(len(values)) + ": " + str(values)
+            #mapping = dict(zip(columns,values))
+            results.append([target_table,mapping]) # adds the target table and final mapping to a results-list
+            
+    return results
+"""
+
 # used to check if a complex parsed parameter is included wholly. 
 # E.g. parameter " Hello, World" could be splitted into ['" Hello', ' World"']
 def check_for_complete_parameter(item):
-    begin_quote = re.match(r"\s*(\"|'|`)", item)    # looks if the string begins with a quote
-    end_quote = re.search(r"\s*(\"|'|`)$", item)    # looks if the string ends in a quote
+    begin_quote = re.match(r"\s*(?<!\\)(\"|'|`)", item)    # looks if the string begins with a quote
+    end_quote = re.search(r"(?<!\\)(\"|'|`)\s*$", item)    # looks if the string ends in a quote
     if begin_quote == None and end_quote == None:
         return True
-    elif (begin_quote != None and end_quote != None) and begin_quote.group(0) == end_quote.group(0):
+    elif (begin_quote != None and end_quote != None) and begin_quote.group(0).strip() == end_quote.group(0).strip():
         return True
     else:
         return False
-            
+
+#checks whether we have a matching number of "organic" brackets 
+# -> the query forms a full line and a newline should terminate it
+def count_brackets(line):
+    quotes = len(re.findall(r"(?<!\\)\'", line))
+    double_quotes = len(re.findall(r"(?<!\\)\"", line))
+    ticks = len(re.findall(r"(?<!\\)\`", line))
+    left_parentheses = len(re.findall(r"(?<!\\)\(", line))
+    left_curlybrackets = len(re.findall(r"(?<!\\)\{", line))
+    left_squarebrackets = len(re.findall(r"(?<!\\)\[", line))
+    right_parentheses = len(re.findall(r"(?<!\\)\)", line))
+    right_curlybrackets = len(re.findall(r"(?<!\\)\}", line))
+    right_squarebrackets = len(re.findall(r"(?<!\\)\]", line))
+    if  (quotes % 2 == 0 and 
+        double_quotes % 2 == 0 and 
+        ticks % 2 == 0 and 
+        left_parentheses == right_parentheses and
+        left_curlybrackets == right_curlybrackets and
+        left_squarebrackets == right_squarebrackets):
+        # print "*==\n\n" + line
+        return True
+        
+    else:
+        return False
+
+
 def main():
     args = sys.argv[1]
     cut_queries_from_file(args)
     # print args
     
-main()
+# main()
---
+++ src/python/insert_translator.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# insert_translator.py
+# attempts to translate old mediawiki 1.4 database-edits into 1.5 syntax
+
+# mostly copied from insert_parser.py
+import sys, re
+from insert_parser import *
+
+def cut_queries_from_file(file):
+    file = open(file,'r')
+    queries = []
+    query= ""
+    transaction_number = None
+    keep_on_reading = 1
+    while keep_on_reading == 1:
+            
+        where = file.tell()
+        line = file.readline()
+        if not line:
+            time.sleep(1)
+            file.seek(where)
+            # we can terminate reading here, for example
+            # keep_on_reading == 1
+        else:
+            # check if we begin a new transaction
+            if re.match(r"\s+\d+\sQuit\t\n", line):
+                print line
+                print (parse_updates(query))
+                query = ""
+            query = query + line
+
+# detect if the query is one we want to intercept
+def check_for_cur_query(queries):
+    for parsed_query in queries:
+        if parsed_query[0] == 'cur':
+            print "cur -query:"
+            print str(parsed_query)
+
+def main():
+    
+    print sys.argv[1]
+    cut_queries_from_file(sys.argv[1])
+    
+main()
+
