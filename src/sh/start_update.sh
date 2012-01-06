670cd08b36b1b019009c155fd2d4056048d16943
Eetu Korhonen <eetu.korhonen@gmail.com>
2012-01-07 00:46:37 +0100

    the scripts to initialize upgrade start to take shape

--- src/sh/start_update.sh
+++ src/sh/start_update.sh
@@ -1,4 +1,13 @@
+# Start the update. Run this on the replicated MW1.4->MW1.5 environment
+
 # First, load the configuration variables:
-source configs.conf
+source configs.replica
+# check the first line number 
+first_line_number=$(wc -l $logfile |tr " " "\n"|head -1)
 
+# gather log-file
+#ssh $username@$original_address "tail -f ${logfile} -n +${first_line_number}" |tee $(local_log_cache)
+# maybe should parallelize to create a single executable?
+((ssh $aws_original_hostname "tail -f ${logfile} -n +${first_line_number}" > $local_log_cache) &)
 
+# remember to terminate that ssh+tail process from memory
---
+++ src/sh/std_update_mwiki14-15.sh
@@ -0,0 +1,3 @@
+# download and do the standard update to mediawiki 1.5
+# may need to be run as superuser, depending on your access-rights
+svn co https://svn.wikimedia.org/viewvc/mediawiki/tags/REL1_4_15/phase3 $HOME/mwiki15
