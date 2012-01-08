d25cd324e9899b0adc44dfdb645060a8d1398880
Eetu Korhonen <eetu.korhonen@gmail.com>
2012-01-08 02:06:51 +0100

    fixed mediawiki url

--- src/sh/std_update_mwiki14-15.sh
+++ src/sh/std_update_mwiki14-15.sh
@@ -1,4 +1,4 @@
 # download and do the standard update to mediawiki 1.5
 # may need to be run as superuser, depending on your access-rights
 cd ~/mw15_update/
-svn co https://svn.wikimedia.org/viewvc/mediawiki/tags/REL1_4_15/phase3 $PWD/mwiki15
+svn co https://svn.wikimedia.org/svnroot/mediawiki/tags/REL1_4_15/phase3 $PWD/mwiki15
